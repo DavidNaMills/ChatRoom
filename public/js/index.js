f78ec3b16bee01e462914e3271674e6c6350e039
David Mills <David Mills>
2017-04-15 15:35:11 +0800

    add form for messages and display new ones

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,6 @@
 	var socket = io();
 		socket.on('connect', function(){
-			console.log("client connected");	
-			
-			
-			socket.emit('newMessage', {			//server->client
-				From: "David",
-				text: "no cookies",
-				createdAt: new Date()
-			});
-			
-			
+			console.log("client connected");		
 		});
 		
 		socket.on('disconnect', function(){
@@ -17,9 +8,43 @@
 		});
 		
 		
-		
-	
-		
-		socket.on('newMessage', function(message){
+		socket.on('newUser', function(message){
 			console.log(message);
 		});
+	
+		socket.on('connected', function(message){
+			console.log(message);
+		});
+		
+		socket.on('newMessage', function(message){
+			//console.log(message);
+			
+			var li = jQuery('<li></li>');
+			var t = message.From+": "+message.text;
+			li.text(t);
+			
+			jQuery('#messages').append(li);
+			
+		});
+		
+		
+	/*socket.emit('createMessage', {			//server->client
+		From: "David",
+		text: "no cookies"
+	}, function(data){
+		
+		console.log(data);
+		
+	});*/
+	
+	
+	jQuery('#message-form').on('submit', function(e){
+		e.preventDefault();
+		
+		socket.emit('createMessage',{
+			From: 'User',
+			text: jQuery('[name=message]').val()
+		}, function(){});
+	});
+	
+	
--- server/server.js
+++ server/server.js
@@ -4,18 +4,38 @@ const http = require ('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3001;
+var {generateMessage} = require('./utils/messaging.js');
 const app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+//const 
+
 io.on('connection', (socket)=>{
 	console.log("new user connected");
+	
+	socket.emit('newMessage', generateMessage('Admin', 'Welcome to the Chat app!'));
+	socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has joined'));
+	
+	
 	
 	socket.on('disconnect',()=>{
 		console.log("user disconnected");	
 	});
+	
+
+	
+	var From;
+	var text;
+	var createdAt;
+	
+	
+	socket.on('createMessage', (message, callback)=>{
+		console.log('in createMessage');
+		io.emit('newMessage', generateMessage(message.From, message.text));
+		callback('this is from the server');
+	});	
 	
 });
 
