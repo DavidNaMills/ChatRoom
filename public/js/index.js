7dc8a9eb47d97ffaea3777cfa43790444bee7f53
David Mills <David Mills>
2017-04-17 21:43:36 +0800

    added mustachejs for templates

--- public/js/index.js
+++ public/js/index.js
@@ -18,28 +18,28 @@
 		
 		socket.on('newMessage', function(message){
 			var formattedTime = moment(message.createdAt).format('h:mm a');
+			var template = jQuery('#messageTemplate').html();
+			var html = Mustache.render(template, {
+				From: message.From,
+				text: message.text,
+				createdAt: formattedTime
+			});
 			
-			var li = jQuery('<li></li>');
-			var t = message.From+" "+formattedTime+": "+message.text;
-			li.text(t);
-			
-			jQuery('#messages').append(li);
-			
+			jQuery('#messages').append(html);
 		});
 		
 	
 
 		socket.on('newLocationMessage', function(message){
 			var formattedTime = moment(message.createdAt).format('h:mm a');
-			var li = jQuery('<li></li>');
-			var a = jQuery('<a target="_blank">My current location</a>');
+			var template = jQuery('#locationMessageTemplate').html();
+			var html = Mustache.render(template, {
+				From: message.From,
+				url: message.url,
+				createdAt: formattedTime
+			});
 			
-			var t = message.From+" "+formattedTime+": ";
-			li.text(t);
-			a.attr('href', message.url);
-			
-			li.append(a);
-			jQuery('#messages').append(li);
+			jQuery('#messages').append(html);
 		});
 
 	
